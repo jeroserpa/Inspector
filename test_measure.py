30bf449c414a1334bf06bce3ce388ebc5706dcba
Jero <jeroserpa@gmail.com>
2020-07-31 18:20:37 +0200

    succes-measuring

---
+++ test_measure.py
@@ -0,0 +1,132 @@
+import numpy as np                        # fundamental package for scientific computing
+import matplotlib.pyplot as plt           # 2D plotting library producing publication quality figures
+import pyrealsense2 as rs                 # Intel RealSense cross-platform open-source API
+import cv2 as cv,cv2
+import time
+
+
+
+
+def filters_init():#initializates filters
+    #creates a dictionary with the filters and configures its parameters
+    filters = {}
+    filters["decimation"]=rs.decimation_filter()
+
+    filters["depth_to_disparity"]=rs.disparity_transform(True)
+    
+    filters["spatial"]=rs.spatial_filter()
+    filters["spatial"].set_option(rs.option.filter_magnitude, 5)
+    filters["spatial"].set_option(rs.option.filter_smooth_alpha, 1)
+    filters["spatial"].set_option(rs.option.filter_smooth_delta, 50)
+    filters["spatial"].set_option(rs.option.holes_fill, 3)
+    
+    filters["disparity_to_depth"]=rs.disparity_transform(False)
+    
+    filters["hole_filling"]=rs.hole_filling_filter()
+
+    return filters
+
+
+def get_filtered(pipe,filters): #gets filtered and original frames from camera
+    #recieves the pipe to get the trqnsmision and a dictionary with the corresponding filters
+    #and implements them
+    frameset = pipe.wait_for_frames()
+    frame = frameset.get_depth_frame()
+    frame_original=frame
+    #I can't excecute the following with a for because dictionaries aren't ordered
+    frame = filters["decimation"].process(frame)
+    frame = filters["depth_to_disparity"].process(frame)
+    frame = filters["spatial"].process(frame)
+    #frame = temporal.process(frame)
+    frame = filters["disparity_to_depth"].process(frame)
+    frame = filters["hole_filling"].process(frame)
+    
+    return frame,frame_original,frameset
+
+
+def get_factor(gray_depth,frame_f,value=200): #returns conversion factor between real depth and grayscale and the value usedto calculate it (you can specify it), by default is 200 because of the noise
+
+    value_indexes = np.where(gray_depth == value)
+    listOfCoordinates= list(zip(value_indexes[0], value_indexes[1]))
+    while not listOfCoordinates:
+        value=value-1
+        value_indexes = np.where(gray_depth == value)
+        listOfCoordinates= list(zip(value_indexes[0], value_indexes[1]))
+        
+    point= listOfCoordinates[0]
+    y_p = point[0]
+    x_p = point[1]
+    point_depth = frame_f.as_depth_frame().get_distance((int(x_p)), (int(y_p)))
+    factor = point_depth/value
+
+    return(factor,value)
+
+def factor_error_m(frame_f,factor):
+    R_depth = frame_f.as_depth_frame().get_distance((int(w/2)), (int(h/2)))
+    E_depth = gray_depth[(int(h/2)), (int(w/2))]*factor
+    error = R_depth-E_depth
+    return error
+
+def main():
+
+    # Setup:
+    pipe = rs.pipeline()
+    cfg = rs.config()
+
+    # Start streaming
+    cfg.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    #cfg.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+    colorizer = rs.colorizer(3)#config colorizer type 3=black to white
+
+    # Start streaming 
+    pipe.start(cfg)
+    
+   
+    # Store next frameset for later processing:
+    while True:
+        
+        frame_f,original,frameset = get_filtered(pipe,filters_init())
+
+        colorized_depth = np.asanyarray(colorizer.colorize(frame_f).get_data())
+        colorized_depth = cv.GaussianBlur(colorized_depth,(9,9),0)
+
+        gray_depth = cv.cvtColor(colorized_depth,cv.COLOR_BGR2GRAY)
+        colorized_depth_o = np.asanyarray(colorizer.colorize(original).get_data())
+        
+        h,w = gray_depth.shape #240x320
+        
+        
+        factor,used_val =get_factor(gray_depth,frame_f)
+
+        #colorized_depth = cv2.circle(colorized_depth, (x_max,y_max), 10,  (255,0,0),thickness=2)   
+        
+        test_point=(300,100)
+        depth_intrin = frame_f.profile.as_video_stream_profile().intrinsics
+        R_depth = frame_f.as_depth_frame().get_distance((int(300)), (int(100)))
+        
+        
+        depth_point = rs.rs2_deproject_pixel_to_point(depth_intrin,[300,100], R_depth)#intrinsics, pixel coord (y,x) and depth coord
+        print(depth_intrin)
+        print(depth_point)
+        
+        point = rs.rs2_project_point_to_pixel(depth_intrin,depth_point)
+        print(point)
+
+        #colorized_depth = cv2.circle(colorized_depth, (300,100), 10,  (255,0,0),thickness=2)   
+        cv.rectangle(img,(384,0),(510,128),(0,255,0),3)
+        cv.imshow("normal",colorized_depth)
+        
+
+        ret,binary_1 = cv.threshold(gray_depth,0.25/factor,255,cv.THRESH_BINARY)
+        cv.imshow("binary",binary_1)
+
+        
+        cv.waitKey(1)
+
+        
+    pipe.stop()
+
+
+if __name__ == '__main__':
+    main()
